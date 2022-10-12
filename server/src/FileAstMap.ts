b9f703b6e285304a1b295b96dfefb19c321b2b6d
Anders Pedersen <hot415@hotmail.com>
2022-10-12 02:18:12 +0200

    Update FileAstMap.ts

--- server/src/FileAstMap.ts
+++ server/src/FileAstMap.ts
@@ -1,26 +1,56 @@
 import { URI, Utils } from 'vscode-uri';
+import parser, { AutoItParser, FunctionDeclaration, IncludeFileNameLiteral, Program, VariableDeclaration, VariableStatement } from "autoit3-pegjs";
 
-export type Maps<DataType> = {
-    [uri: string]: {
-        data: DataType,
-        counter: number,
-        includes: string[],
-    }
+export type FileRef = {
+    uri: string,
+    /** Indicates include-once */
+    once: boolean,
 }
 
-export default class FileAstMap<DataType extends {body?: Array<any>}, Parser extends Function> {
-    protected maps: Maps<DataType> = {};
-    protected parser: Parser;
+export type Includes = {
+    global: FileRef[],
+    scopes: {
+        [scope: string]: FileRef[],
+    },
+}
 
-    constructor(parser: Parser) {
+//export type Identifier = VariableDeclaration|VariableStatement|FunctionDeclaration;
+export type Identifier = VariableDeclaration|FunctionDeclaration;
+
+export type Identifiers = {
+    global: {[name: string]: Identifier},
+    scopes: {[scope: string]: {[name: string]: Identifier}}
+}
+
+export type Maps = {
+    [uri: string]: {
+        data: Program,
+        /** reference counter */
+        counter: number,
+        //includes: string[],
+        includes: Includes,
+        identifiers: Identifiers,
+        scopes: {[scope: string]: FunctionDeclaration}
+    };
+}
+
+export default class FileAstMap {
+    protected maps: Maps = {};
+    protected parser: AutoItParser;
+
+    constructor() {
         this.parser = parser;
     }
 
-    exists(uri): boolean {
+    parse(input: string, uri?: string): Program {
+        return this.parser.parse(input, {grammarSource: uri});
+    }
+
+    exists(uri: string): boolean {
         return this.maps.hasOwnProperty(uri);
     }
 
-    add(uri: string, data: DataType): void {
+    add(uri: string, data: Program): void {
         if (this.exists(uri)) {
             this.maps[uri].counter += 1;
             // update the data
@@ -29,24 +59,50 @@ export default class FileAstMap<DataType extends {body?: Array<any>}, Parser ext
             this.maps[uri] = {
                 data: data,
                 counter: 1,
-                includes: [],
+                includes: {
+                    global: [],
+                    scopes: {},
+                },
+                identifiers: {
+                    global: {},
+                    scopes: {},
+                },
+                scopes: {},
             };
         }
 
-        this.maps[uri].data = data;
+        data.body?.forEach(item => {
+            switch (item.type) {
+                case "FunctionDeclaration":
+                    this.maps[uri].identifiers.global[item.id.name.toLowerCase()] = item;
+                    break;
+                case "VariableDeclaration":
+                    item.declarations.forEach(declaration => {
+                        this.maps[uri].identifiers.global['$' + declaration.id.name.toLowerCase()] = declaration;
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
+
         //this.addIncludes(uri);
     }
 
-    addIncludes(uri) {
-        const previousIncludes = this.maps[uri].includes;
-        const includes = this.maps[uri].data?.body?.filter((node: {type: string}) => node.type === "IncludeStatement").map(includeUri => this.resolveIncludePath(uri, includeUri)) ?? [];
+    addIncludes(uri: string) {
+        const previousIncludes = this.maps[uri].includes.global.map(fileRef => fileRef.uri); //FIXME
+        const includes = this.maps[uri].data.body.filter((node): node is {
+            type: "IncludeStatement",
+            file: IncludeFileNameLiteral,
+            location: any,
+        } => node.type === "IncludeStatement").map((includeUri) => this.resolveIncludePath(uri, includeUri.file)) ?? [];
 
         this.difference(previousIncludes, includes).forEach(previousInclude => this.release(previousInclude));
         this.difference(includes, previousIncludes).forEach(include => {
             this.addInclude(include);
         });
 
-        this.maps[uri].includes = includes;
+        this.maps[uri].includes.global = includes.map(uri => ({uri: uri, once: false})); //FIXME
     }
 
     addInclude(uri) {
@@ -78,10 +134,89 @@ export default class FileAstMap<DataType extends {body?: Array<any>}, Parser ext
         }
     }
 
-    get(uri: string): DataType {
+    get(uri: string): Program {
         if (!this.exists(uri)) {
             throw new Error(`URI not found in map: ${uri}`);
         }
         return this.maps[uri].data;
     }
+
+    /** Get first declaration statement for matching identifier */
+    getIdentifierDeclarator(uri: string, identifier: Identifier): FunctionDeclaration|VariableDeclaration|null {
+        if (!this.exists(uri)) {
+            throw new Error(`URI not found in map: ${uri}`);
+        }
+        //const scope = this.getScopeAt(uri, identifier.location.start.line, identifier.location.start.line); //FIXME: scope support
+
+        const identifiers = this.maps[uri].identifiers.global;
+        const _identifier = Object.keys(identifiers ?? {}).find(identifierIndex => {
+            const _identifier = identifiers[identifierIndex];
+            return _identifier.id === identifier.id;
+        })
+
+        return _identifier !== undefined ? identifiers[_identifier] : null;
+    }
+
+    /** get identifier object based on cursor position */
+    getIdentifierAt(uri: string, line: number, column: number): Identifier|null {
+        if (!this.exists(uri)) {
+            throw new Error(`URI not found in map: ${uri}`);
+        }
+        //FIXME: object properties with dot notation support.
+        const scope = this.getScopeAt(uri, line, column); //FIXME: iterate document includes, until match is found?
+        const scopeIdentifiers = this.maps[uri].identifiers.scopes[scope?.id.name ?? ""];
+        const scopeIdentifier = Object.keys(scopeIdentifiers ?? {}).find(scopeIdentifier => {
+            const _scopeIdentifier = scopeIdentifiers[scopeIdentifier] as {location?: {start: {line: number, column: number}, end: {line: number, column: number}}};
+            if (_scopeIdentifier !== undefined && _scopeIdentifier.location !== undefined) {
+                if (_scopeIdentifier.location.start.line <= line && _scopeIdentifier.location.end.line >= line) {
+                    if (_scopeIdentifier.location.start.column <= column && _scopeIdentifier.location.end.column >= column) {
+                        return true;
+                    }
+                }
+            }
+        });
+
+        if (scopeIdentifier !== undefined) {
+            return scopeIdentifiers[scopeIdentifier] ?? null;
+        }
+
+        const identifiers = this.maps[uri].identifiers.global;
+        const identifier = Object.keys(identifiers ?? {}).find(identifier => {
+            const _identifier = identifiers[identifier];
+            if (_identifier !== undefined && _identifier.location !== undefined) {
+                if (_identifier.location.start.line <= line && _identifier.location.end.line >= line) {
+                    if (_identifier.location.start.column <= column && _identifier.location.end.column >= column) {
+                        return true;
+                    }
+                }
+            }
+        })
+        return identifier !== undefined ? identifiers[identifier] : null;
+    }
+
+    /** get scope id based on cursor position */
+    getScopeAt(uri: string, line: number, column: number) {
+        if (!this.exists(uri)) {
+            throw new Error(`URI not found in map: ${uri}`);
+        }
+
+        const map = this.maps[uri];
+        const scopes = map.scopes;
+        const scope = Object.keys(scopes).find(scope => {
+            const _scope = scopes[scope] as {location?: {start: {line: number, column: number}, end: {line: number, column: number}}};
+            if (_scope !== undefined && _scope.location !== undefined) {
+                if (_scope.location.start.line <= line && _scope.location.end.line >= line) {
+                    if (_scope.location.start.column <= column && _scope.location.end.column >= column) {
+                        return true;
+                    }
+                }
+            }
+        });
+
+        return scope !== undefined ? scopes[scope] : null;
+    }
+}
+
+export class AutoIt {
+    //
 }
