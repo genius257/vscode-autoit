0b8816230b584b5561248f6fa0bc2fff4f4e54d0
Anders Pedersen <hot415@hotmail.com>
2022-10-15 01:43:30 +0200

    Update FileAstMap.test.ts

--- server/src/FileAstMap.test.ts
+++ server/src/FileAstMap.test.ts
@@ -7,6 +7,14 @@ test("getIdentifierAt", function () {
     fileAstMap.add("test", parser.parse("$x = 1"));
     
     const identifier = fileAstMap.getIdentifierAt("test", 1, 1);
+
+    expect(identifier).not.toBeNull();
+});
+
+test("getIdentifierAt nested", function () {
+    fileAstMap.add("test", parser.parse("Const $x = 1\nConst $y = $x"));
     
+    const identifier = fileAstMap.getIdentifierAt("test", 2, 12);
+
     expect(identifier).not.toBeNull();
 });
