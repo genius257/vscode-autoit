6970b51d0df2eef1b1515b5f3382a0242c86f10f
Anders Pedersen <hot415@hotmail.com>
2023-01-06 22:52:16 +0100

    Fixed issue with node branch walking condition not being altered after supporting the new NodeFilterAction enum as return
    I KNOW this fix is not the best, functionality wise AND code duplication wise.
    It will be addressed at a later time if need be.

--- server/src/autoit/Script.ts
+++ server/src/autoit/Script.ts
@@ -436,22 +436,47 @@ export default class Script {
                 return NodeFilterAction.Continue;
         }
 
+        let status: NodeFilterAction = NodeFilterAction.Continue;
         switch (node.type) {
             case "ArrayDeclaration":
                 return this.filterNestedNodes(node.elements, fn, matches);
             case "AssignmentExpression":
             case "BinaryExpression":
-                return this.filterNestedNode(node.left, fn, matches) ?? this.filterNestedNode(node.right, fn, matches);
+                status = this.filterNestedNode(node.left, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.right, fn, matches);
+                return status;
             case "CallExpression":
-                return this.filterNestedNode(node.callee, fn, matches) ?? this.filterNestedNodes(node.arguments, fn, matches);
+                status = this.filterNestedNode(node.callee, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.arguments, fn, matches);
+                return status;
             case "ConditionalExpression":
-                return this.filterNestedNode(node.test, fn, matches) ?? this.filterNestedNode(node.consequent, fn, matches) ?? this.filterNestedNode(node.alternate, fn, matches);
+                status = this.filterNestedNode(node.test, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.consequent, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.alternate, fn, matches);
+                return status;
             case "ContinueCaseStatement":
                 break;
             case "ContinueLoopStatement":
                 return this.filterNestedNode(node.level, fn, matches);
             case "DoWhileStatement":
-                return this.filterNestedNode(node.test, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                status = this.filterNestedNode(node.test, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             case "EmptyStatement":
                 break;
             case "ExitLoopStatement":
@@ -461,11 +486,48 @@ export default class Script {
             case "ExpressionStatement":
                 return this.filterNestedNode(node.expression, fn, matches);
             case "ForInStatement":
-                return this.filterNestedNode(node.left, fn, matches) ?? this.filterNestedNode(node.right, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                status = this.filterNestedNode(node.left, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.right, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             case "ForStatement":
-                return this.filterNestedNode(node.id, fn, matches) ?? this.filterNestedNode(node.init, fn, matches) ?? this.filterNestedNode(node.test, fn, matches) ?? this.filterNestedNode(node.update, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                status = this.filterNestedNode(node.id, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.init, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.test, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.update, fn, matches)
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             case "FunctionDeclaration":
-                return this.filterNestedNode(node.id, fn, matches) ?? this.filterNestedNodes(node.params, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                //FIXME: the fix made here should be made for all multi property nodes cases, here and in other filter functions.
+                //FIXME: assert status and return the most relevant or stop when reaching stop enum values and return that?
+                status = this.filterNestedNode(node.id, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.params, fn, matches)
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             case "Identifier":
                 break;
             case "IfStatement":
@@ -483,17 +545,32 @@ export default class Script {
             case "Literal":
                 break;
             case "LogicalExpression":
-                return this.filterNestedNode(node.left, fn, matches) ?? this.filterNestedNode(node.right, fn, matches);
+                status = this.filterNestedNode(node.left, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.right, fn, matches);
+                return status;
             case "Macro":
                 break;
             case "MemberExpression":
-                return this.filterNestedNode(node.object, fn, matches) ?? this.filterNestedNode(node.property, fn, matches);
+                status = this.filterNestedNode(node.object, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.property, fn, matches);
+                return status;
             case "MultiLineComment":
                 break;
             case "NotExpression":
                 return this.filterNestedNode(node.value, fn, matches);
             case "Parameter":
-                return this.filterNestedNode(node.id, fn, matches) ?? this.filterNestedNode(node.init, fn, matches);
+                status = this.filterNestedNode(node.id, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.init, fn, matches);
+                return status;
             case "PreProcStatement":
                 break;
             case "RedimExpression":
@@ -504,29 +581,64 @@ export default class Script {
             case "ReturnStatement":
                 return this.filterNestedNode(node.value, fn, matches);
             case "SelectCase":
-                return this.filterNestedNode(node.tests, fn, matches) ?? this.filterNestedNodes(node.consequent, fn, matches);
+                status = this.filterNestedNode(node.tests, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.consequent, fn, matches);
+                return status;
             case "SelectStatement":
                 return this.filterNestedNodes(node.cases, fn, matches);
             case "SingleLineComment":
                 break;
             case "SwitchCase":
-                return this.filterNestedNodes(node.tests, fn, matches) ?? this.filterNestedNodes(node.consequent, fn, matches);
+                status = this.filterNestedNodes(node.tests, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.consequent, fn, matches);
+                return status;
             case "SwitchCaseRange":
-                return this.filterNestedNode(node.from, fn, matches) ?? this.filterNestedNode(node.to, fn, matches);
+                status = this.filterNestedNode(node.from, fn, matches)
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.to, fn, matches);
+                return status;
             case "SwitchStatement":
-                return this.filterNestedNode(node.discriminant, fn, matches) ?? this.filterNestedNodes(node.cases, fn, matches);
+                status = this.filterNestedNode(node.discriminant, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.cases, fn, matches);
+                return status;
             case "UnaryExpression":
                 return this.filterNestedNode(node.argument, fn, matches)
             case "VariableDeclaration":
                 return this.filterNestedNodes(node.declarations, fn, matches);
             case "VariableDeclarator":
-                return this.filterNestedNode(node.id, fn, matches) ?? this.filterNestedNode(node.init, fn, matches);
+                status = this.filterNestedNode(node.id, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNode(node.init, fn, matches);
+                return status;
             case "VariableIdentifier":
                 break;
             case "WhileStatement":
-                return this.filterNestedNode(node.test, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                status = this.filterNestedNode(node.test, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             case "WithStatement":
-                return this.filterNestedNode(node.object, fn, matches) ?? this.filterNestedNodes(node.body, fn, matches);
+                status = this.filterNestedNode(node.object, fn, matches);
+                if (status === NodeFilterAction.Stop || status === NodeFilterAction.StopAndSkip) {
+                    return status;
+                }
+                status = this.filterNestedNodes(node.body, fn, matches);
+                return status;
             default:
                 //@ts-expect-error
                 throw new Error("Unsupported type: " + node.type);
