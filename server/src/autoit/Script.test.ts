01434502a5bc194a0ccce24ffc4eba341c98122d
Anders Pedersen <hot415@hotmail.com>
2023-01-15 23:40:08 +0100

    Added test for Script.getNodesAt

---
+++ server/src/autoit/Script.test.ts
@@ -0,0 +1,13 @@
+import Script from "./Script";
+
+describe("Script", function () {
+    const script = new Script(`If 1 Then
+    Exit 1
+EndIf`);
+
+    test("getNodesAt", function() {
+        expect(script.getNodesAt({character: 1, line: 0}).reverse()[0].type).toBe("IfStatement");
+        expect(script.getNodesAt({character: 6, line: 1}).reverse()[0].type).toBe("ExitStatement");
+        expect(script.getNodesAt({character: 3, line: 2}).reverse()[0].type).toBe("IfStatement");
+    });
+});
