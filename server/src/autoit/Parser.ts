909ca904ab2c9cf0cd1cb441eb36b5a971250791
Anders Pedersen <hot415@hotmail.com>
2024-01-30 19:32:47 +0000

    Removed unused import value

--- server/src/autoit/Parser.ts
+++ server/src/autoit/Parser.ts
@@ -1,5 +1,4 @@
 import parser, { ArgumentList, ArrayDeclarationElementList, AssignmentExpression, CaseClause, CaseValueList, DefaultClause, EnumDeclaration, EnumDeclarationList, FormalParameter, FormalParameterList, Initialiser, LocationRange, Program, RedimIdentifierExpression, SelectCaseClause, SourceElement, Statement, StatementList, SwitchCaseValue, SyntaxError, VariableDeclaration, VariableDeclarationList } from "autoit3-pegjs";
-import { Range } from "vscode-languageserver";
 
 export default class Parser {
     public static parse(input: string, grammarSource: string|undefined): Program {
