d941e9efb032063820781127b50a1e8d387ed654
Anders Pedersen <hot415@hotmail.com>
2025-01-16 21:41:49 +0100

    Added inline markdown formatter class for inline tag formatting

---
+++ server/src/autoit/docBlock/DocBlock/Tags/Formatter/InlineMarkdownFormatter.ts
@@ -0,0 +1,22 @@
+import BaseTag from "../BaseTag";
+import Link from "../Link";
+import MarkdownFormatter from "./MarkdownFormatter";
+
+export default class InlineMarkdownFormatter extends MarkdownFormatter {
+    public override format(tag: BaseTag): string {
+            let body = tag.toString();
+            switch (true) {
+                case tag instanceof Link:
+                    const description = tag.getDescription()?.render(this) ?? "";
+    
+                    if (description === "") {
+                        body = `<${tag.getLink()}>`;
+                    } else {
+                        body = `[${description}](${tag.getLink()})`;
+                    }
+                    break;
+            }
+    
+            return body;
+    }
+}
