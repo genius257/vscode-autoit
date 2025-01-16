d1876a6a0bbff1825ac340301eef9aaac1612ae3
Anders Pedersen <hot415@hotmail.com>
2025-01-16 21:42:37 +0100

    Added markdown desciption and factory for outputting markdown description flavored text

---
+++ server/src/autoit/docBlock/DocBlock/MarkdownDescription.ts
@@ -0,0 +1,20 @@
+import vsprintf from "locutus/php/strings/vsprintf";
+import Description from "./Description";
+import Formatter from "./Tags/Formatter";
+import InlineMarkdownFormatter from "./Tags/Formatter/InlineMarkdownFormatter";
+
+export default class MarkdownDescription extends Description {
+    public override render(formatter: Formatter | null = null): string {
+        if (this.getTags().length === 0) {
+            return vsprintf(this.getBodyTemplate(), []);
+        }
+
+        if (formatter === null) {
+            formatter = new InlineMarkdownFormatter();
+        }
+
+        const tags = this.getTags().map((tag) => formatter.format(tag));
+
+        return vsprintf(this.getBodyTemplate(), tags);
+    }
+}
