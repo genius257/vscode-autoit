66770856f19ca71b32f41bdb858feca15e30a199
Anders Pedersen <hot415@hotmail.com>
2025-02-21 01:32:17 +0100

    Changed from using non-null-assertion operator to using optional chaning instead

--- server/src/autoit/Script.test.ts
+++ server/src/autoit/Script.test.ts
@@ -7,9 +7,9 @@ describe('Script', function () {
 EndIf`);
 
     test('getNodesAt', function () {
-        expect(script.getNodesAt({ character: 1, line: 0 }).reverse()[0]!.type).toBe('IfStatement');
-        expect(script.getNodesAt({ character: 6, line: 1 }).reverse()[0]!.type).toBe('ExitStatement');
-        expect(script.getNodesAt({ character: 3, line: 2 }).reverse()[0]!.type).toBe('IfStatement');
+        expect(script.getNodesAt({ character: 1, line: 0 }).reverse()[0]?.type).toBe('IfStatement');
+        expect(script.getNodesAt({ character: 6, line: 1 }).reverse()[0]?.type).toBe('ExitStatement');
+        expect(script.getNodesAt({ character: 3, line: 2 }).reverse()[0]?.type).toBe('IfStatement');
     });
 });
 
