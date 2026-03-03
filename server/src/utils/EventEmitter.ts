5b1ecf79c32a5fd77d50e8c9dbb325bb199e24d8
Anders Pedersen <hot415@hotmail.com>
2026-03-03 17:27:24 +0100

    Added EventEmitter class to utillities

---
+++ server/src/utils/EventEmitter.ts
@@ -0,0 +1,51 @@
+export type EventMap = Record<string | symbol, unknown>;
+
+export type Listener<T> = (payload: T) => void;
+
+export default class EventEmitter<Events extends EventMap> {
+    protected listeners: {
+        [K in keyof Events]?: Set<Listener<Events[K]>>;
+    } = {};
+
+    on<K extends keyof Events>(event: K, listener: Listener<Events[K]>): void {
+        if (!this.listeners[event]) {
+            this.listeners[event] = new Set();
+        }
+        this.listeners[event]!.add(listener);
+    }
+
+    off<K extends keyof Events>(event: K, listener: Listener<Events[K]>): void {
+        this.listeners[event]?.delete(listener);
+
+        // Optional cleanup to avoid empty sets accumulating
+        if (this.listeners[event]?.size === 0) {
+            delete this.listeners[event];
+        }
+    }
+
+    emit<K extends keyof Events>(event: K, payload: Events[K]): void {
+        this.listeners[event]?.forEach((listener) => {
+            listener(payload);
+        });
+    }
+
+    once<K extends keyof Events>(
+        event: K,
+        listener: Listener<Events[K]>,
+    ): void {
+        const wrapper: Listener<Events[K]> = (payload) => {
+            this.off(event, wrapper);
+            listener(payload);
+        };
+
+        this.on(event, wrapper);
+    }
+
+    clear<K extends keyof Events>(event?: K): void {
+        if (event) {
+            delete this.listeners[event];
+        } else {
+            this.listeners = {};
+        }
+    }
+}
