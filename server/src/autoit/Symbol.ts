1b7be192954a637e82796ec58303923ea619b556
Anders Pedersen <hot415@hotmail.com>
2026-03-15 17:14:00 +0100

    Added Symbol, Delcaration and Reference classes

---
+++ server/src/autoit/Symbol.ts
@@ -0,0 +1,62 @@
+import { AutoIt3, LocationRange } from 'autoit3-pegjs';
+import DocBlock from './docBlock/DocBlock';
+import { URI } from 'vscode-uri';
+import Scope from './Scope';
+
+type Node =
+    AutoIt3.Macro | AutoIt3.VariableIdentifier | AutoIt3.Identifier;
+
+export default class Symbol {
+    protected node: Node;
+
+    public constructor(node: Node) {
+        this.node = node;
+    }
+}
+
+export type SymbolType = 'variable' | 'function' | 'class' | 'constant';
+
+export class Declaration {
+    public readonly name: string;
+    public readonly type: SymbolType;
+    public readonly uri?: URI;
+    public readonly range: LocationRange;
+    public readonly scope: Scope;
+    public readonly docBlock?: DocBlock;
+    protected references = new Set<Symbol>();
+
+    public constructor(arg: {
+        name: string,
+        type: SymbolType,
+        uri?: URI,
+        range: LocationRange,
+        scope: Scope,
+        docBlock?: DocBlock,
+    }) {
+        this.name = arg.name;
+        this.type = arg.type;
+        this.uri = arg.uri;
+        this.range = arg.range;
+        this.scope = arg.scope;
+        this.docBlock = arg.docBlock;
+    }
+
+    public addReference(symbol: Symbol) {
+        this.references.add(symbol);
+    }
+
+    public removeReference(symbol: Symbol) {
+        this.references.delete(symbol);
+    }
+
+    public replaceReference(oldSymbol: Symbol, newSymbol: Symbol) {
+        this.references.delete(oldSymbol);
+        this.references.add(newSymbol);
+    }
+
+    public getReferences(): ReadonlySet<Symbol> {
+        return this.references;
+    }
+}
+
+export class Reference {}
