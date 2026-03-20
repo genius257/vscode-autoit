c69474c5f180c209b7ba1ce9372e9efc8872adee
Anders Pedersen <hot415@hotmail.com>
2026-03-20 16:43:29 +0100

    Added properties to track scirpt symbols on scope class.

--- server/src/autoit/Scope.ts
+++ server/src/autoit/Scope.ts
@@ -7,13 +7,23 @@ export default class Scope {
     public readonly range?: LocationRange;
     public readonly parent: Scope | null;
 
+    protected declarations: Map<string, Node>;
+    protected assignments: Map<string, Node>;
+    protected references: Map<string, Node>;
+
     public constructor(
         range?: LocationRange,
         uri?: URI,
+        declarations: typeof this.declarations = new Map(),
+        assignments: typeof this.assignments = new Map(),
+        references: typeof this.references = new Map(),
         parent: Scope | null = null,
     ) {
         this.uri = uri;
         this.range = range;
+        this.declarations = declarations;
+        this.assignments = assignments;
+        this.references = references;
         this.parent = parent;
         this.id = `${uri}:${range?.start.line}:${range?.start.column}`;
     }
