e7451dc814554ac66986da91a530c996bea0778a
Anders Pedersen <hot415@hotmail.com>
2026-07-31 16:24:46 +0200

    Changed debounce function to be more type-safe

--- server/src/utils/debounce.ts
+++ server/src/utils/debounce.ts
@@ -1,11 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export default function debounce<F extends Function>(cb: F, delay = 250) {
-    let timeout;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function debounce<T extends (...args: any[]) => void>(cb: T, delay = 250) {
+    let timeout: ReturnType<typeof setTimeout> | undefined;
 
-    return (((...args) => {
+    return (...args: Parameters<T>) => {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
             cb(...args);
         }, delay);
-    }) as unknown) as F;
+    };
 }
