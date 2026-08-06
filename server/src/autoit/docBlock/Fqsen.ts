be316d4f5a629422f29056de2cf737516ccc30d6
Anders Pedersen <hot415@hotmail.com>
2026-08-06 08:11:20 +0200

    Changed empty Fqsen stub into a parsed, validated model with toString and getName

--- server/src/autoit/docBlock/Fqsen.ts
+++ server/src/autoit/docBlock/Fqsen.ts
@@ -1,7 +1,39 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class Fqsen {
+    /** full quallified class name */
+    private fqsen: string;
+
+    /** name of the element without path. */
+    private name: string;
+
     // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-unused-vars
     public constructor(fqsen: string) {
-        //
+        const regex = /^\\([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\\]*)?(?:::\$?([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*))?(?:\(\))?$/;
+
+        const matches = fqsen.match(regex);
+
+        if (!matches) {
+            throw new Error(`"${fqsen}" is not a valid Fqsen.`);
+        }
+
+        this.fqsen = fqsen;
+
+        if (matches[2] !== undefined) {
+            this.name = matches[2];
+        } else if (fqsen === '\\') {
+            this.name = '';
+        } else {
+            const parts = fqsen.split('\\');
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const lastName = parts[parts.length - 1]!;
+            this.name = lastName.replace(/\(\)$/, '');
+        }
+    }
+
+    public toString(): string {
+        return this.fqsen;
+    }
+
+    public getName(): string {
+        return this.name;
     }
 }
